b2936759a052055e43b136c8e3a582203c196d2e
Ram Iyer <ramky.iyer@yahoo.com>
2015-01-26 17:17:42 -0800

    adding nodemodule to generate the fotune cookie

--- meadowlark.js
+++ meadowlark.js
@@ -1,3 +1,5 @@
+var fortune = require('./lib/fortune.js');
+
 var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').
@@ -17,17 +19,8 @@ app.get('/', function(req, res) {
   res.render('home');
 });
 
-var fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
-];
-
 app.get('/about', function(req, res) {
-  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render('about', { fortune: randomFortune });
+  res.render('about', { fortune: fortune.getFortune() } );
 });
 
 // Custom 404 page
